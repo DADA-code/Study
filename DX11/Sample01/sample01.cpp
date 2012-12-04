78b67f7d9ccc9c0ddc6a4971924b50a4a1b53497
sumio <lekirakka@gmail.com>
2012-12-05 02:02:37 +0900

    sample01...

--- DX11/Sample01/sample01.cpp
+++ DX11/Sample01/sample01.cpp
@@ -17,8 +17,8 @@
 HINSTANCE g_Instance_Handle = NULL;
 HWND      g_Window_Handle   = NULL;
 
-WCHAR     g_App_Title[] = L"Direct3D 11 Sample";
-WCHAR     g_Window_Class_Name= L"D3D11S01";
+WCHAR     g_App_Title[]         = L"Direct3D 11 Sample";
+WCHAR     g_Window_Class_Name[] = L"D3D11S01";
 
 //起動時の描画領域サイズ
 SIZE      g_Initial_Client_Erea_Size = { 640, 480 };  // ウィンドウのクライアント領域
@@ -41,9 +41,52 @@ ID3D11Texture2D*        g_Depth_Stencil_Texture;
 ID3D11DepthStencilView* g_Depth_Stencil_View; 
 
 
-
+// 関数定義
 LRESULT CALLBACK MainWndProc(HWND window_handle, UINT message, UINT parameter, LONG long_paramter);
 HRESULT          InitBackBuffer(void);
 
 
-//HRESULT InitApp
+HRESULT InitApp(HINSTANCE instance_handle) {
+	// アプリケーションのインスタンスハンドルを保存
+	g_Instance_Handle = instance_handle;
+
+	// ウインドウクラスの登録
+	WNDCLASS window_class;
+	window_class.style         = CS_HREDRAW | CS_VREDRAW;
+	window_class.lpfnWndProc   = (WNDPROC)MainWndProc;
+	window_class.cbClsExtra    = 0;
+	window_class.cbWndExtra    = 0;
+	window_class.hInstance     = instance_handle;
+//	window_class.hIcon         = LoadIcon(instance_handle, (LPCTSTR)IDI_ICON1);
+	window_class.hCursor       = LoadCursor(NULL, IDC_ARROW);
+	window_class.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+	window_class.lpszMenuName  = NULL;
+	window_class.lpszClassName = g_Window_Class_Name;
+
+	if(!RegisterClass( &window_class)) {
+		return DXTRACE_ERR(L"InitApp", GetLastError());
+	}
+
+	// メインウィンドウ作成
+	RECT rect;
+	rect.top    = 0;
+	rect.left   = 0;
+	rect.right  = g_Initial_Client_Erea_Size.cx;
+	rect.bottom =  g_Initial_Client_Erea_Size.cy;
+	AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, TRUE);
+
+	g_Window_Handle = CreateWindow(g_Window_Class_Name, g_App_Title,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		rect.right - rect.left, rect.bottom - rect.top,
+		NULL, NULL, instance_handle, NULL);
+	if(g_Window_Handle == NULL) {
+		return DXTRACE_ERR(L"InitApp", GetLastError());
+	}
+
+	// ウィンドウ表示
+	ShowWindow(g_Window_Handle, SW_SHOWNORMAL);
+	UpdateWindow(g_Window_Handle);
+
+	return S_OK;
+}
